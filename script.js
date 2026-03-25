e2b128b200465a52dd5ec1111f2c255569777da5
Nusrat-anjum <258597654+Nusrat-anjum@users.noreply.github.com>
2026-03-26 00:08:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function searchJob(){
+
+let job = document.getElementById("search").value;
+
+alert("Searching for: " + job);
+
+}
+
+function loginUser(){
+
+alert("Login successful");
+
+return true;
+
+}
+
+function registerUser(){
+
+alert("Registration successful");
+
+return true;
+
+}
+
+function applyJob(){
+
+alert("Application submitted");
+
+return true;
+
+}
+function toggleDarkMode(){
+document.body.classList.toggle("dark");
+}
